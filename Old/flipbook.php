1dfce5cd77d55d089458c68c993a3a228f0120be
keko <kekojeda@gmail.com>
2019-08-29 21:24:08 -0300

    CV version entreganda

---
+++ Old/flipbook.php
@@ -0,0 +1,3 @@
+
+
+<?php echo do_shortcode('[flipbook pdf="http://loopstudio.co/pdf/Brochure-LOOP-Visualization-2019.pdf"]'); ?>
---
+++ Old/sin porcentajes/calculadora.js
@@ -0,0 +1,23 @@
+function sumar(nro1, nro2){
+    var sumar = nro1+nro2;
+    return sumar;
+}
+
+function restar(nro1, nro2){
+    var restar = nro1-nro2;
+    return restar;
+}
+
+function multiplicar(nro1, nro2){
+    var multiplicar = nro1*nro2;
+    return multiplicar;
+}
+function dividir(nro1, nro2){
+    var dividir = nro1/nro2;
+    return dividir;
+}
+
+sumar(3,3);
+restar(3,3);
+multiplicar(13,3);
+dividir(3,3);
